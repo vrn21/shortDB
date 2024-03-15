3ba5a818ee1aa7daa18400d650c90b4ae16582b6
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-03-15 20:04:45 +0530

    reponding to ping

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
-use std::net::TcpListener;
-
+use std::io::{BufRead, BufReader,Write};
+use std::net::{TcpListener, TcpStream};
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:6379").expect("Server failed to start");
     for stream in listener.incoming() {
         match stream{
-            Ok(_stream) =>{
+            Ok(stream) =>{
                 println!("Server working");
+                handle_connection(stream);
             }
             Err(_e) => {
                 println!("Something went wrong couldnt match");
             }
         }
-        println!("TCP listener started listening");
+        println!("TCP listener waiting");
+    }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    println!("Handling connection");
+    let buf_reader = BufReader::new(&stream);
+    let mut lines = buf_reader.lines();
+
+    match lines.next() {
+        Some(line) => {
+            match line {
+                Ok(request_line) => {
+                    if request_line == "*1\r\n$4\r\nping\r\n" {
+                        stream.write(b"+PONG\r\n").expect("Couldn't write into the stream");
+                    }
+                },
+                Err(e) => {
+                    eprintln!("Error reading line: {}", e);
+                    return;
+                }
+            }
+        },
+        None => {
+            eprintln!("No lines to read from the stream");
+            return;
+        }
     }
 }
