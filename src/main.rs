0c63224b9ed7fb5e6b75bfbe52d7dde113a47ffa
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-03-19 08:55:47 +0530

    multiple reponses

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
-use std::io::{BufRead, BufReader,Write};
+use std::io::{BufRead, BufReader,Write,Read};
 use std::net::{TcpListener, TcpStream};
+
+use bytes::buf;
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:6379").expect("Server failed to start");
     for stream in listener.incoming() {
         match stream{
-            Ok(stream) =>{
+            Ok(mut stream) =>{
                 println!("Server working");
-                handle_connection(stream);
+                loop {
+                    //handle_connection(&mut stream);
+                    let data = b"+PONG\r\n"; 
+                    stream.write(data).expect("Couldn't write into the stream");
+                    //let buf_reader = BufReader::new(&mut stream);
+                    let mut buf_reader = [0;256];
+                    let n = stream.read(&mut buf_reader).expect("Couldn't read from the stream");
+                    let buf_reader = &buf_reader[..n];
+                    let buf_reader = std::str::from_utf8(&buf_reader).expect("Couldnt convert to Stringx");
+                    println!("Read from stream {:?}", &buf_reader[..n]);
+
+                }
             }
             Err(_e) => {
                 println!("Something went wrong couldnt match");
@@ -16,11 +29,11 @@ fn main(){
     }
 }
 
-fn handle_connection(stream:  TcpStream){
-    println!("Handling connection");
-    let buf_reader = BufReader::new(&stream);
-    let mut lines = buf_reader.lines();
-    let data = b"+PONG\r\n"; 
-    let mut stream = &stream;
-    stream.write(data).expect("Couldn't write into the stream");
-}
+// fn handle_connection(stream:  &mut TcpStream){
+//     println!("Handling connection");
+//     let buf_reader = BufReader::new(stream);
+//     let mut lines = buf_reader.lines();
+//     let data = b"+PONG\r\n"; 
+//     let mut stream = &stream;
+//     stream.write(data).expect("Couldn't write into the stream");
+// }
